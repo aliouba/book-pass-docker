a46aedbd0c565a918f054842468f40a6557d4732
BA Aliou <alioubafr@gmail.com>
2017-09-20 18:27:57 +0200

    Update index.js

--- chapitre1/DockerfileSimpleNodeJS/index.js
+++ chapitre1/DockerfileSimpleNodeJS/index.js
@@ -1,11 +1,6 @@
 var http = require('http');
-//loads http module
 var app=http.createServer(function (req, res) {
-//creates server
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  //sets the right header and status code
   res.end('Hello World\n');
-  //outputs string with line end symbol
 }).listen(8080, "0.0.0.0");
-//sets port and IP address of the server
 console.log('Server running at http://127.0.0.1:8080');
